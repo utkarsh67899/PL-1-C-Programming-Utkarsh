b912fe4d2586bbe7a255897ff0e2797e9d322161
utkarsh67899 <sonsaleutkarsh@gmail.com>
2026-09-16 00:48:39 +0530

    Add files via upload

---
+++ UBS12.c
@@ -0,0 +1,24 @@
+// Program (12) -> Write a program to determine whether a given number is positive, negative, or zero.
+
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int num;
+printf("Enter a number: "); 
+scanf("%d", &num);
+
+	if (num > 0) 
+	{
+	printf("The number is Positive.");
+	}
+	else if (num < 0) 
+	{
+	printf("The number is Negative.");
+	}
+	else
+	{
+	printf("The number is Zero.");
+	}
+getch();
+}
