70a77617eda010cd600b9de36d5b600e0062ae57
utkarsh67899 <sonsaleutkarsh@gmail.com>
2026-09-16 00:47:07 +0530

    Add files via upload

---
+++ UBS11.c
@@ -0,0 +1,21 @@
+/*Program (11) -> Write a program to check whether a given number is even or odd*/
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int num; 
+printf("Enter your number: "); scanf("%d", &num);
+}
+
+if (num % 2 == 0)
+{
+printf("The number is Even.");
+} 
+
+else
+{
+printf("The number is Odd.");
+}
+
+getch();
